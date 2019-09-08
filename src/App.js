0a7e658ae7a1cbf9f81012d6d96ac8a1afe5b112
JuanL <juan_lc5@hotmail.com>
2019-09-08 14:50:54 -0500

    Started using basic props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function HelloWorld() {
+function HelloWorld(props) {
   return (
-    <div id="hello">Hello World !!!</div>
+    <div id="hello">
+      <h3>{props.title}</h3>
+      {props.myText}
+    </div>
   );
 }
 
 function App() {
   return (
     <div >
-      This is my component <HelloWorld/> <HelloWorld/> <HelloWorld/>
+      This is my component
+      <HelloWorld myText="Hello World !!" title="Title"/>
+      <HelloWorld myText="Hello 1 !!"/>
+      <HelloWorld myText="Hello 2 !!"/>
     </div>
   );
 }
