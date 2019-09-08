addaac2ec4b4540b94d5625b7621f7d1277366e1
JuanL <juan_lc5@hotmail.com>
2019-09-08 15:12:25 -0500

    Started using States

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import './App.css';
 
-function HelloWorld(props) {
+/*function HelloWorld(props) {
   return (
     <div id="hello">
       <h3>{props.title}</h3>
       {props.myText}
     </div>
   );
+}*/
+
+class HelloWorld extends React.Component {
+  state = {
+    show: true
+  }
+
+  toogleShow = () => {
+    this.setState({show:!this.state.show})
+  }
+
+  render() {
+    if (this.state.show) {
+      return(
+        <div id="hello">
+          <h3>{this.props.title}</h3>
+          {this.props.myText}
+          <button onClick={this.toogleShow}>Hide</button>
+        </div>
+      );
+    } else {
+      return(
+        <div>
+          <h1>Empty Component</h1>
+          <button onClick={this.toogleShow}>Show</button>
+        </div>
+      );
+    }
+  }
 }
 
 function App() {
