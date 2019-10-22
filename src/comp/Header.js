03170a760fa3102d5ad55408e3a63b9715e5faa0
JuanL <juan_lc5@hotmail.com>
2019-10-21 20:40:42 -0500

    Improved header class a bit

--- src/comp/Header.js
+++ src/comp/Header.js
@@ -5,14 +5,14 @@ class Header extends React.Component {
   render() {
     return(
       <div>
-        <div style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: 50}} className="header">
+        <div style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: 20}} className="header">
           <div className="title-box">
             <h2>{this.props.exerciseHeader}</h2>
           </div>
-          <img alt="Oops, algo salió mal" style={{width: 350, height: 175, marginTop: 'auto', marginBottom: 'auto', marginRight: '10%'}} src="./img/logo.PNG"/>
+          <img alt="Oops, algo salió mal" style={{width: 330, height: 155, marginTop: 'auto', marginBottom: 'auto', marginRight: '10%'}} src="./img/logo.PNG"/>
         </div>
         <div style={{width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', marginBottom: 100}}>
-          <div className="instruction-box" style={{width: '50%', height: 120}}>
+          <div className="instruction-box" style={{height: 120}}>
             <p style={{width: '70%', textAlign: 'center'}}>
               {this.props.instructions}
             </p>
