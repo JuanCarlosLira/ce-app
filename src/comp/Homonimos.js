bf93d72cbf8dc45dd18c26d0d9c864f11eb5a162
JuanL <juan_lc5@hotmail.com>
2019-10-22 20:12:54 -0500

    Added Full Table in Homonimos

--- src/comp/Homonimos.js
+++ src/comp/Homonimos.js
@@ -129,8 +129,20 @@ class Homonimos extends React.Component {
       bank: [],
       maya: [],
       malla: [],
-      b1: [],
-      b2: []
+      baya: [],
+      valla: [],
+      cocer: [],
+      coser: [],
+      casa: [],
+      caza: [],
+      errar: [],
+      herrar: [],
+      cegar: [],
+      segar: [],
+      vaso: [],
+      bazo: [],
+      reusar: [],
+      rehusar: []
     };
 
     this.state.imgs.forEach(i => {
@@ -169,9 +181,9 @@ class Homonimos extends React.Component {
           </div>
           <br />
 
-          <table style={{ width: "100%" }}>
+          <table style={{ tableLayout: "fixed", width: "90%", borderRadius: "5px",  borderCollapse: "collapse", background: "red", display: "inline-block" }}>
             <tr>
-              <td>
+              <td style={{width: "200px", height: "150px", background: "#99ccff"}}>
                 <div
                   className="maya"
                   id="container"
@@ -182,14 +194,180 @@ class Homonimos extends React.Component {
                   <span className="task-header">MAYA</span><br/> {imgs.maya}{" "}
                 </div>{" "}
               </td>
-              <td>
-                Y
+              <td style={{width: "200px", height: "150px",  background: "#6699ff"}}>
+                <div
+                  className="malla"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "malla")}
+                >
+                  {" "}
+                  <span className="task-header">MALLA</span><br/> {imgs.malla}{" "}
+                </div>{" "}
               </td>
-              <td>
-                Z
+              <td style={{width: "200px", height: "150px",  background: "#99ccff"}}>
+                <div
+                  className="baya"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "baya")}
+                >
+                  {" "}
+                  <span className="task-header">BAYA</span><br/> {imgs.baya}{" "}
+                </div>{" "}
               </td>
-              <td>
-                W
+              <td style={{width: "200px", height: "150px",  background: "#6699ff"}}>
+                <div
+                  className="valla"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "valla")}
+                >
+                  {" "}
+                  <span className="task-header">VALLA</span><br/> {imgs.valla}{" "}
+                </div>{" "}
+              </td>
+              <td style={{width: "200px", height: "150px",  background: "#99ccff"}}>
+                <div
+                  className="cocer"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "cocer")}
+                >
+                  {" "}
+                  <span className="task-header">COCER</span><br/> {imgs.cocer}{" "}
+                </div>{" "}
+              </td>
+              <td style={{width: "200px", height: "150px",  background: "#6699ff"}}>
+                <div
+                  className="coser"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "coser")}
+                >
+                  {" "}
+                  <span className="task-header">COSER</span><br/> {imgs.coser}{" "}
+                </div>{" "}
+              </td>
+            </tr>
+            <tr>
+              <td style={{width: "200px", height: "150px", background: "#6699ff"}}>
+                <div
+                  className="casa"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "casa")}
+                >
+                  {" "}
+                  <span className="task-header">CASA</span><br/> {imgs.casa}{" "}
+                </div>{" "}
+              </td>
+              <td style={{width: "200px", height: "150px",  background: "#99ccff"}}>
+                <div
+                  className="caza"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "caza")}
+                >
+                  {" "}
+                  <span className="task-header">CAZA</span><br/> {imgs.caza}{" "}
+                </div>{" "}
+              </td>
+              <td style={{width: "200px", height: "150px",  background: "#6699ff"}}>
+                <div
+                  className="errar"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "errar")}
+                >
+                  {" "}
+                  <span className="task-header">ERRAR</span><br/> {imgs.errar}{" "}
+                </div>{" "}
+              </td>
+              <td style={{width: "200px", height: "150px",  background: "#99ccff"}}>
+                <div
+                  className="herrar"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "herrar")}
+                >
+                  {" "}
+                  <span className="task-header">HERRAR</span><br/> {imgs.herrar}{" "}
+                </div>{" "}
+              </td>
+              <td style={{width: "200px", height: "150px",  background: "#6699ff"}}>
+                <div
+                  className="cegar"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "cegar")}
+                >
+                  {" "}
+                  <span className="task-header">CEGAR</span><br/> {imgs.cegar}{" "}
+                </div>{" "}
+              </td>
+              <td style={{width: "200px", height: "150px",  background: "#99ccff"}}>
+                <div
+                  className="segar"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "segar")}
+                >
+                  {" "}
+                  <span className="task-header">SEGAR</span><br/> {imgs.segar}{" "}
+                </div>{" "}
+              </td>
+            </tr>
+            <tr>
+              <td style={{width: "200px", height: "150px", background: "#99ccff"}}>
+                {" "}
+              </td>
+              <td style={{width: "200px", height: "150px",  background: "#6699ff"}}>
+                <div
+                  className="vaso"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "vaso")}
+                >
+                  {" "}
+                  <span className="task-header">VASO</span><br/> {imgs.vaso}{" "}
+                </div>{" "}
+              </td>
+              <td style={{width: "200px", height: "150px",  background: "#99ccff"}}>
+                <div
+                  className="bazo"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "bazo")}
+                >
+                  {" "}
+                  <span className="task-header">BAZO</span><br/> {imgs.bazo}{" "}
+                </div>{" "}
+              </td>
+              <td style={{width: "200px", height: "150px",  background: "#6699ff"}}>
+                <div
+                  className="reusar"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "reusar")}
+                >
+                  {" "}
+                  <span className="task-header">REUSAR</span><br/> {imgs.reusar}{" "}
+                </div>{" "}
+              </td>
+              <td style={{width: "200px", height: "150px",  background: "#99ccff"}}>
+                <div
+                  className="rehusar"
+                  id="container"
+                  onDragOver={e => this.onDragOver(e)}
+                  onDrop={e => this.onDrop(e, "rehusar")}
+                >
+                  {" "}
+                  <span className="task-header">REHUSAR</span><br/> {imgs.rehusar}{" "}
+                </div>{" "}
+              </td>
+              <td style={{width: "200px", height: "150px",  background: "#6699ff"}}>
+                {" "}
               </td>
             </tr>
           </table>
@@ -200,6 +378,35 @@ class Homonimos extends React.Component {
 }
 
 export default Homonimos;
+
+/*
+<table style={{ tableLayout: "fixed", width: "90%", borderRadius: "5px",  borderCollapse: "collapse", background: "red", display: "inline-block" }}>
+  <tr>
+    <td>
+      <div
+        className="cocer"
+        id="container"
+        onDragOver={e => this.onDragOver(e)}
+        onDrop={e => this.onDrop(e, "cocer")}
+      >
+        {" "}
+        <span className="task-header">COCER</span><br/> {imgs.cocer}{" "}
+      </div>{" "}
+    </td>
+    <td>
+      <div
+        className="coser"
+        id="container"
+        onDragOver={e => this.onDragOver(e)}
+        onDrop={e => this.onDrop(e, "coser")}
+      >
+        {" "}
+        <span className="task-header">COSER</span><br/> {imgs.coser}{" "}
+      </div>{" "}
+    </td>
+  </tr>
+</table>
+*/
 
 /*<div className="container-drag">
   <Header/>
